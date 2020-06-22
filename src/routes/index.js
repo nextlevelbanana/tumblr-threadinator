daf25b6b77a8b128479e6f5caddb7fb156365ca0
Kristy OVERTON <qsgiveaways@gmail.com>
2020-06-21 21:08:45 -0700

    hopefully making it deployable

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
-import express from "express";
+const express = require("express");
 const fs = require("fs");
 const path = require("path")
 const router = express.Router();
 
-router.get('/blog/:name', async (req, res) => {
-    console.log(req.params.name);
-    res.status(200).render('pages/index', {name: req.params.name, 
-    data: JSON.parse(fs.readFileSync(path.resolve(__dirname,`../../${req.params.name}-output.json`)))
-    });
-});
+// router.get('/blog/:name', async (req, res) => {
+//     console.log(req.params.name);
+//     res.status(200).render('pages/index', {name: req.params.name, 
+//     data: JSON.parse(fs.readFileSync(path.resolve(__dirname,`../../${req.params.name}-output.json`)))
+//     });
+// });
 
-export default router;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-import express from "express";
-import compression from "compression";
-import index from "./routes/index";
-import path from "path";
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const fs = require("fs");
+
 
 // Server var
 const app = express();
+const router = express.Router();
 
 // View engine setup
 app.set("views", path.join(__dirname,"views"));
@@ -13,9 +15,14 @@ app.set("view engine", "ejs");
 // Middleware
 app.use(compression());
 app.use(express.static(__dirname + "/public"));
-
-//Routes
-app.use("/", index);
+app.get('/blog/:name', async (req, res) => {
+        res.status(200).render('pages/index', {name: req.params.name, 
+        data: JSON.parse(fs.readFileSync(path.resolve(__dirname,`../${req.params.name}-output.json`)))
+        });
+    });
+app.get('/', (req,res) => {
+    res.send("<html><body><p>Welcome to the Tumblr Threadinator!</p><p>All pages can be found under /blog/{blogname}.</p></body></html>")
+})
 
 const port = process.env.PORT || 3000;
 
